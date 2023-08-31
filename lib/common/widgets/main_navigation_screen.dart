c8b38f001bf02e6bd4f8194b0643a175ae217b40
Saehanseul <saehanseul@gmail.com>
2023-08-31 13:30:03 +0900

    Feat: add write modal

--- lib/common/widgets/main_navigation_screen.dart
+++ lib/common/widgets/main_navigation_screen.dart
@@ -3,9 +3,11 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
 import 'package:thread_clone/common/widgets/nav_tab.dart';
 import 'package:thread_clone/constants/breakpoints.dart';
+import 'package:thread_clone/constants/gaps.dart';
 import 'package:thread_clone/constants/sizes.dart';
 import 'package:thread_clone/features/etc/screens/etc_screen.dart';
 import 'package:thread_clone/features/home/screens/home_screen.dart';
+import 'package:thread_clone/features/home/widgets/ProfileCircleImage.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   static const String routeName = "mainNavigation";
@@ -34,6 +36,176 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  void onWritingTap(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        String text = '';
+        return StatefulBuilder(
+          builder: (context, setState) {
+            return FractionallySizedBox(
+              heightFactor: 0.9,
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
+                  child: Column(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(Sizes.size16),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                GestureDetector(
+                                  onTap: () => Navigator.pop(context),
+                                  child: const Text(
+                                    "Cancel",
+                                    style: TextStyle(
+                                      fontSize: Sizes.size16,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          const Text(
+                            'New Thread',
+                            style: TextStyle(
+                              fontSize: Sizes.size16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: Sizes.size16,
+                            ),
+                            child: Column(
+                              children: [
+                                const ProfileCircleImage(
+                                  url: "assets/images/profile_image1.jpeg",
+                                  size: Sizes.size48,
+                                ),
+                                Gaps.v10,
+                                SizedBox(
+                                  height: Sizes.size96,
+                                  child: VerticalDivider(
+                                    thickness: 2,
+                                    color: Colors.grey.shade300,
+                                  ),
+                                ),
+                                Gaps.v10,
+                                const Opacity(
+                                  opacity: 0.5,
+                                  child: ProfileCircleImage(
+                                    url: "assets/images/profile_image1.jpeg",
+                                    size: Sizes.size24,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 300,
+                            width: MediaQuery.of(context).size.width - 110,
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                top: Sizes.size16,
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    "jane_mobbin",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  // Gaps.v20,
+                                  TextField(
+                                    onChanged: (value) {
+                                      print(value);
+                                      setState(() {
+                                        text = value;
+                                      });
+                                    },
+                                    autofocus: true,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Start a thread...',
+                                      border: InputBorder.none,
+                                    ),
+                                  ),
+                                  Gaps.v10,
+                                  const FaIcon(
+                                    FontAwesomeIcons.paperclip,
+                                    color: Colors.grey,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Spacer(), // Pushes the button to the bottom
+                      SafeArea(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: Sizes.size16,
+                            vertical: Sizes.size16,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                "Anyone can reply",
+                                style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                ),
+                              ),
+                              const Spacer(),
+                              Text(
+                                "Post",
+                                style: TextStyle(
+                                  color: text.isNotEmpty
+                                      ? Colors.blue
+                                      : Colors.blue.shade200,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: Sizes.size16,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
   }
 
   @override
@@ -96,7 +268,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                   icon: FontAwesomeIcons.penToSquare,
                   selectedIcon: FontAwesomeIcons.penToSquare,
                   isSelected: _selectedIndex == 2,
-                  onTab: () => _onTap(2),
+                  onTab: () => onWritingTap(context),
                   selectedIndex: _selectedIndex,
                 ),
                 NavTab(
